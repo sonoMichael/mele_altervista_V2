481b331991783261717daace95b5ee8be1144c4f
sonoMichael <zhaomichael004@gmail.com>
2024-04-05 19:55:14 +0200

    created about.html

--- mele_altervista/view.py
+++ mele_altervista/view.py
@@ -3,6 +3,21 @@ from mele_altervista import app
 
 home_btns = ["about", "projects", "contact"]
 
+profile_par = "BASTAAAAAAA"
+
+jobs = [
+    {
+        "title": "t1",
+        "period": "prx",
+        "detail": "drx"
+    },
+    {
+        "title": "t1 academy",
+        "period": "psg",
+        "detail": "dg"
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -10,3 +25,9 @@ def home():
                            title = "Portfolio",
                            num_btns = len(home_btns),
                            home_btns = home_btns)
+
+@app.route("/about")
+def about():
+    return render_template("about.html",
+                           desc = profile_par,
+                           jobs = jobs)
