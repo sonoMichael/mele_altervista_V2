11733f648933eec206792bb0c640406903207e52
sonoMichael <zhaomichael004@gmail.com>
2024-04-12 21:39:20 +0200

    adjusted db images

--- mele_altervista/database.py
+++ mele_altervista/database.py
@@ -1,46 +1,45 @@
 from sqlalchemy import create_engine, text
 import os
-from PIL import Image
-import io
+import base64
 
 
 engine = create_engine(os.getenv("DB_CONNECTION_STRING"))
 
-try:
-    def load_description_from_db():
-        with engine.connect() as conn:
-            print("CONNECTED")
-            result = conn.execute(text("select * from description"))
-            description = []
-            for row in result.all():
-                description.append(row._mapping)
-            return description
-        
-    def load_jobs_from_db():
-        with engine.connect() as conn:
-            result = conn.execute(text("select * from jobs"))
-            jobs = []
-            for row in result.all():
-                jobs.append(row._mapping)
-            return jobs
+def load_description_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from description"))
+        description = []
+        for row in result.all():
+            desc = row._asdict()
+            desc['image'] = base64.b64encode(desc['image']).decode('utf-8')
+            description.append(desc)
+        return description
+    
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = []
+        for row in result.all():
+            jobs.append(row._asdict())
+        return jobs
 
-    def load_projects_from_db():
-        with engine.connect() as conn:
-            result = conn.execute(text("select * from projects"))
-            projects = []
-            for row in result.all():
-                projects.append(row._mapping)
-            return projects
+def load_projects_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from projects"))
+        projects = []
+        for row in result.all():
+            project = row._asdict()
+            project['image'] = base64.b64encode(project['image']).decode('utf-8')
+            projects.append(project)
+        return projects
 
-    def load_contacts_from_db():
-        print("Inside function")
-        with engine.connect() as conn:
-            result = conn.execute(text("select * from contacts"))
-            print("Initialize result")
-            contacts = []
-            for row in result.all():
-                contacts.append(row._mapping)
-                # contacts[row]['image'] = Image.open(io.BytesIO(row['image']))
-            return contacts
-except:
-    print("ERRORE")
+def load_contacts_from_db():
+    print("Inside function")
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from contacts"))
+        contacts = []
+        for row in result.all():
+            contact = row._asdict()
+            contact['image'] = base64.b64encode(contact['image']).decode('utf-8')
+            contacts.append(contact)
+        return contacts
